a3394979e946cc88076c8e38c3f9a5a77043e559
danpro3 <118496439+danpro3@users.noreply.github.com>
2024-12-10 11:37:43 -0500

    online update 10_trailheads.py

--- 2024/10_trailheads.py
+++ 2024/10_trailheads.py
@@ -1,7 +1,8 @@
 # %% 
 # load input
 import re
-filestr = open('inputs/input_10_test.txt','r').read()
+filestr = '89010123\n78121874\n87430965\n96549874\n45678903\n32019012\n01329801\n10456732'
+# filestr = open('inputs/input_10_test.txt','r').read()
 # filestr = open('inputs/input_10.txt','r').read()
 # filestr = open('inputs/input_06_test.txt','r').read()
 grid = filestr.splitlines()
@@ -10,7 +11,6 @@ grid = filestr.splitlines()
 # print(f'startidx = {startidx}, start = {start}')
 _=[print(x) for x in grid]
 # %%
-# main
 from heapq import heapify, heappush, heappop
 rows = len(grid)
 cols = len(grid[0])
@@ -25,67 +25,46 @@ def find_trailheads():
     print(trailheads)
     return trailheads
 
-# def find_hikes(loc):
-#     move = {'U':(-1,0), 'D':(1,0), 'L':(0,-1), 'R':(0,1)}
-    
-#     todo = []
-#     if 
-
-#     while todo:
+def find_hikes(trailhead):
+    # move = {'U':(-1,0), 'D':(1,0), 'L':(0,-1), 'R':(0,1)}
+    r,c = trailhead
+    score = []
+    todo = []
+    heappush(todo, (-1, r, c)
+    while todo:
+        move(todo)
+    return score
 
 def V(r,c): # return the value of the grid
     return grid[r][c]
 
 def move(todo):
-    dist, r, c, dir = heappop(todo)
-    # if we've been here before, end this thread
-    key = (r, c, dir)
-    # if key in cache:
-    #     # print(r,c,dir,'in cache')
-    #     return todo
-    # else:
-    #     # add this state to the cache
-    #     cache[key] = 0
-    #     cache_grid[r][c] = '#'
-
+    dist, r, c = heappop(todo)
     if grid[r][c] == '9':  # found one!
         score.append(1)
         return todo
     # check up
     if r>0 and V(r-1,c) < V(r,c)+1:
-        heappush(todo, (dist+1, r-1, c, 'U'))
+        heappush(todo, (dist+1, r-1, c)
     # check down
     if r<rows-1 and V(r+1,c) < V(r,c)+1:
-        heappush(todo, (dist+1, r+1, c, 'D'))
+        heappush(todo, (dist+1, r+1, c)
     # check left
     if c>0 and V(r,c-1) < V(r,c)+1:
-        heappush(todo, (dist+1, r, c-1, 'L'))
+        heappush(todo, (dist+1, r, c-1)
     # check right
     if c<cols-1 and V(r,c+1) < V(r,c)+1:
-        heappush(todo, (dist+1, r, c+1, 'R'))
+        heappush(todo, (dist+1, r, c+1)
     return(todo)
-
 
 # main
 trailheads = find_trailheads()
+total_score = 0
 for trailhead in trailheads:
-    score = [] # tally up how many hikes
-
-    # start
     r,c = trailhead
-    # cache = {}   # list of all places been: (row, col, dir)
-    # todo = start_the_todo(S)
-    todo = []
-    # heapify(todo)
-    heappush(todo, (-1, r, c, ''))
-
-    print(todo)
-    while len(todo) > 0:# and todo[0][1:3] != E:
-        # print(todo[0][1:3])
-        # print(todo)
-        todo = move(todo)
-        # print(f'dist = {todo[0][0]}')
-
+    score = find_hikes(trailhead)
+    total_score = += score
+print(f'total score = {total_score}')
 
 
 
