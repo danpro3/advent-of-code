1945d6e1b6638acc73abaf86ad201bf11f3acc25
Dan Provenzano <danpro3@gmail.com>
2024-12-12 08:30:22 -0500

    day 11 part 1 start

--- 2024/11_garden_fences.py
+++ 2024/11_garden_fences.py
@@ -3,3 +3,20 @@ import re
 grid = open('inputs/input_11_test.txt','r').read().splitlines()
 # grid = open('inputs/input_11.txt','r').read().splitlines()
 _=[print(x) for x in grid]
+print(grid)
+# %%
+
+
+def check_region():
+
+    return 1
+
+
+
+
+regions = {}
+for r,line in enumerate(grid):
+    for c,char in enumerate(line):
+        print(char, grid[r][c])
+
+        
