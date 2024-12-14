c337d6ddc4bbb4a08a73dda1355465fd5ffe98bc
Dan Provenzano <danpro3@gmail.com>
2024-12-14 01:00:08 -0500

    day 11 part 2 complete

--- 2024/11_garden_fences.py
+++ 2024/11_garden_fences.py
@@ -1,9 +1,10 @@
 # %% load input
 # import re
-grid = 'AAAA\nBBCD\nBBCC\nEEEC'
-grid = grid.split('\n')
+# grid = 'AAAA\nBBCD\nBBCC\nEEEC'
+# grid = 'EEEEE\nEXXXX\nEEEEE\nEXXXX\nEEEEE'
+# grid = grid.split('\n')
 # grid = open('inputs/input_11_test.txt','r').read().splitlines()
-# grid = open('inputs/input_11.txt','r').read().splitlines()
+grid = open('inputs/input_11.txt','r').read().splitlines()
 # _=[print(x) for x in grid]
 # print(grid)
 rows = len(grid)
@@ -46,9 +47,9 @@ for R in range(rows):
                 todo = move(todo)
             ID += 1
 price = 0
-for R in regions:
+for ID in regions:
     # print(f'{R}, plot: {regions[R][0][3]}, area: {len(regions[R])}, perimeter: {sum([x[2] for x in regions[R]])}')
-    price += len(regions[R])*sum([x[2] for x in regions[R]])
+    price += len(regions[ID])*sum([x[2] for x in regions[ID]])
 
 print(f'price = {price}')
 
@@ -56,30 +57,46 @@ print(f'price = {price}')
 
 # %%
 
-def get_sides(R):
-    # find the range of the region
-    cells = set()
-    allr = []; allc = []
-    for info in regions[R]:
-        cells.add(info[0:2])
-        allr.append(info[0])
-        allc.append(info[1])
-    rows = (min(allr),max(allr))
-    cols = (min(allc),max(allc))
-    print(f'R: {R}, cells = {cells}, rows = {rows}, cols = {cols}')
+def get_sides(ID):
+    # for each direction, if a cell needs a wall "above",
+    # don't add it if the neighbor to the right needs it.  
+    sides = 0
+    # dirs = [(-1,0), (0,-1), (1,0), (0,1)] # up, left, down, right
+    for info in regions[ID]:
+        r,c,G = [info[i] for i in [0,1,3]]
+        # check for wall above by looking at neighbor right
+        if r-1 < 0 or (r-1 >= 0 and grid[r-1][c] != G): # needs a wall
+            if c+1 == cols or (c+1 < cols and grid[r][c+1] != G) or ( # end of road
+            (c+1 < cols and grid[r][c+1]) == G and (r-1 >= 0 and c+1 < cols and grid[r-1][c+1] == G)):  # region steps up
+                sides += 1
 
-    # scan the region, append a sides score for each cell in the rectangle
-    for r in range(rows[1],rows[2]+1):
-        for c in range(cols[1],cols[2]+1):
-            1
+        # check for wall right by looking at neighbor down
+        if c+1 == cols or (c+1< cols and grid[r][c+1] != G): # needs a wall
+            if r+1 == rows or (r+1 < rows and grid[r+1][c] != G) or ( # end of road 
+            (r+1 < rows and grid[r+1][c]) == G and (r+1 < rows and c+1 < cols and grid[r+1][c+1] == G)):  # region steps up
+                sides += 1
 
+        # check for wall below by looking at neighbor left
+        if r+1 == rows or (r+1 < rows and grid[r+1][c] != G): # needs a wall
+            if c-1 < 0 or (c-1 >= 0 and grid[r][c-1] != G) or ( # end of road
+            (c-1 >= 0 and grid[r][c-1]) == G and (r+1 < rows and c-1 >= 0 and grid[r+1][c-1] == G)):  # region steps up
+                sides += 1
 
+        # check for wall left by looking at neighbor up
+        if c-1 < 0 or (c-1 >= 0 and grid[r][c-1] != G): # needs a wall
+            if r-1 < 0 or (r-1 >= 0 and grid[r-1][c] != G) or ( # end of road
+            (r-1 >= 0 and grid[r-1][c]) == G and (r-1 >= 0 and c-1 >= 0 and grid[r-1][c-1] == G)):  # region steps up
+                sides += 1
+        # print (r,c,sides)
+    return sides
+# _=[print(x) for x in grid]
 
+# _=[print(f' {ID}  {regions[ID]}') for ID in regions]
+total_price = 0
+for ID in regions:
+    sides = get_sides(ID)
+    area = len(regions[ID])
+    total_price += sides*area
+    # print(f'ID: {ID}, {regions[ID][0][3]}, sides = {sides}, area = {area}')
 
-    return 0#sides
-
-_=[print(f' {R}  {regions[R]}') for R in regions]
-
-for R in regions:
-    sides = get_sides(R)
-    
+print(f'total price = {total_price}')
