83b295806246944fedf125dcb08084f36a771527
Dan Provenzano <danpro3@gmail.com>
2024-12-27 13:30:53 -0500

    day 24 part 1 cleanup

--- 2024/24_logic_gates.py
+++ 2024/24_logic_gates.py
@@ -24,26 +24,26 @@ def valid_output(wires):
     else:
         return False
 
-def run_gate(wires, gates):
+def run_gate(wires, gate):
     # a AND b -> c
     a = gate[0]; instr = gate[1]; b = gate[2]; c = gate[4]
     if wires[a] == None or wires[b] == None:
         return wires
-    if instr == 'AND':
-        wires[c] = wires[a] & wires[b]
-    if instr == 'OR':
-        wires[c] = wires[a] | wires[b]
-    if instr == 'XOR':
-        wires[c] = wires[a] ^ wires[b]
+    wires[c] = eval('wires[a]' + operations[instr] + 'wires[b]')
+    return wires
+
+def init_wires(wires):
+    # initialize the wires
+    for gate in gates:
+        for i in [0,2,4]:
+            # if gate[i] not in wires:
+            if gate[i][0] not in ['x','y']:
+                wires[gate[i]] = None
     return wires
 
 # main
-# build the full list of gates
-for gate in gates:
-    for i in [0,2,4]:
-        if gate[i] not in wires:
-            wires[gate[i]] = None
-
+operations = {'AND':'&', 'OR':'|', 'XOR':'^'}
+wires = init_wires(wires)
 i = 0
 while not valid_output(wires):
     for gate in gates:
@@ -57,6 +57,5 @@ bin_num = [wires[x] for x in Zs]
 print(bin_num)
 bin_str = ''.join([str(x) for x in bin_num])
 # b = '0b'+bin_str
-print(int(bin_str, base=2))
-
+print(f'result = {int(bin_str, base=2)}')
 
