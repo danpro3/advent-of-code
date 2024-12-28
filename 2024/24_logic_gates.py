7a0b3a56e451591dca25bb4326c9ea63728851b1
Dan Provenzano <danpro3@gmail.com>
2024-12-28 15:09:06 -0500

    day 24 part 2 progress

--- 2024/24_logic_gates.py
+++ 2024/24_logic_gates.py
@@ -1,7 +1,8 @@
 # %% input
 
 # filestr = open('inputs/input_24_test.txt','r').read().split('\n\n')
-filestr = open('inputs/input_24.txt','r').read().split('\n\n')
+# filestr = open('inputs/input_24.txt','r').read().split('\n\n')
+filestr = open('inputs/input_24_swaps.txt','r').read().split('\n\n')
 wirestrings = filestr[0].splitlines()
 gates = filestr[1].splitlines()
 for i,gate in enumerate(gates):
@@ -56,6 +57,124 @@ Zs = sorted([x for x in wires if x[0]=='z'], reverse=True)
 bin_num = [wires[x] for x in Zs]
 print(bin_num)
 bin_str = ''.join([str(x) for x in bin_num])
-# b = '0b'+bin_str
 print(f'result = {int(bin_str, base=2)}')
 
+# %% part 2 ---------------------
+def list2num(var):
+    newlist = sorted([x for x in wires if x[0]==var], reverse=True)
+    bin_num = [wires[x] for x in newlist]
+    print(bin_num)
+    bin_str = ''.join([str(x) for x in bin_num])
+    num = int(bin_str, base=2)
+    print(f'result = {num}')
+    return num
+
+def adder(wires):
+    wires = init_wires(wires)
+    i = 0
+    while not valid_output(wires):
+        for gate in gates:
+            wires = run_gate(wires, gate)
+        i += 1
+    print(f'iterations = {i}')
+    return list2num('z')
+
+x = list2num('x')
+y = list2num('y')
+z = adder(wires)
+print(f'addlist(x,y) = {x+y}')
+print(f'addlist(x,y) - (x+y) = {z-x-y}')
+
+# %%
+def find_gate(name,oper):
+    i = -1
+    found = False
+    while not found and i < len(gates)-1:
+        i += 1 
+        if oper in gates[i] and name in gates[i]:
+            if gates[i].index(name) < 3:
+                found = True
+    if not found:
+        print(f'bad wire: {name}')
+        baddies.add(name)
+    return gates[i], found
+
+xlist = sorted([x for x in wires if x[0]=='x'])
+zlist = sorted([x for x in wires if x[0]=='z'])
+baddies =set()
+for i,xbit in enumerate(xlist):
+    # print()
+    # level 1 OXR
+    gate1, found = find_gate(xbit,'XOR')
+    # print(f'xbit: {xbit}, gate1: {gate1}, {found}')
+
+    if xbit == 'x00': # 0th bit
+        if gate1[4] != 'z00':
+            print(f'bad gate1: {gate1}')
+            baddies.add(gate1[4])
+            baddies.add('z00')
+            break
+    else: # needs to go to (^ then done) and (& then | (rest goes with next bit))
+        gate2, found = find_gate(gate1[4],'XOR')
+        # print(f'xbit: {xbit}, gate2: {gate2}, {found}')
+        if not found and gate2[4] != zlist[i]:
+            print(f'bad gate2 (ender): {gate2}')
+            baddies.add(gate2[4])
+            baddies.add(zlist[i])
+
+        gate2, found = find_gate(gate1[4],'AND')
+        # print(f'xbit: {xbit}, gate2: {gate2}, {found}')
+
+        gate3, found = find_gate(gate2[4],'OR')
+        # print(f'xbit: {xbit}, gate3: {gate3}, {found}')
+
+    # level 1 AND
+    gate1, found = find_gate(xbit,'AND')
+    # print(f'xbit: {xbit}, gate1: {gate1}, {found}')
+
+    if xbit == 'x00': # 0th bit
+        gate2, found = find_gate(gate1[4],'XOR')
+        # print(f'xbit: {xbit}, gate1: {gate2}, {found}')
+        if gate2[4] != 'z01':
+            print(f'bad gate2 (ender): {gate2}')
+            baddies.add(gate2[4])
+            baddies.add('z01')
+            break
+        gate2, found = find_gate(gate1[4],'AND')
+        # print(f'xbit: {xbit}, gate1: {gate2}, {found}')
+    else: # needs to go to ^ then &
+        gate3, found = find_gate(gate1[4],'OR')
+        # print(f'xbit: {xbit}, gate3: {gate3}, {found}')
+        if xbit != xlist[-1]: # not last bit
+            gate4, found = find_gate(gate3[4],'XOR')
+            # print(f'xbit: {xbit}, gate4: {gate4}, {found}')
+            if gate4[4] != zlist[i+1]:
+                # print(f'bad gate4 (ender): {gate4}')
+                baddies.add(gate4[4])
+                baddies.add(zlist[i+1])
+                # break
+ 
+        # print(f'{xbit}, baddies: {baddies}')
+        if len(baddies) == 2:
+            print('need to swap {baddies}')
+            break
+
+print(f'{xbit}, baddies: {baddies}')
+
+
+# %% testing
+# keep track of swappers:
+# x06, baddies: {'z07', 'bjm'}
+# x12, baddies: {'hsw', 'z13'}
+# x17, baddies: {'skf', 'z18'}
+# gives 0 error for x+y
+# x44, baddies: {'nvr', 'z27', 'tdd', 'z35', 'z26', 'wkr'}
+# swap nvr, tdd
+# swap wkr, nvr
+
+# bjm,hsw,skf,tdd,wkr,z07,z13,z18 is not right
+
+baddies2 = {'z07', 'bjm','hsw', 'z13','skf','z18','tdd','wkr'}
+print(baddies2)
+password = ','.join(sorted(baddies2))
+print(f'password = {password}')
