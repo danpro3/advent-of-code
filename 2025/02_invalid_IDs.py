3d1f469335d8fac8aaddb7e74daa4f6ab380163f
Dan Provenzano <danpro3@gmail.com>
2025-12-02 14:34:36 -0500

    2025 day 2 part 2 done

--- 2025/02_invalid_IDs.py
+++ 2025/02_invalid_IDs.py
@@ -24,3 +24,34 @@ for i,group in enumerate(groups):
     for ID in IDs:
         sum_invalids += does_it_repeat(ID)
 print(f'sum of invalid IDs = {sum_invalids}')
+
+# %% Part 2
+import re
+
+def does_it_repeat(num):
+    mystr = str(num)
+    mystrlen = len(mystr)
+    # print(mystr)
+    for i in range(mystrlen//2,0,-1):
+        pattern = r'('+mystr[0:i]+')'
+        matches = re.findall(pattern, mystr)
+        sum_repeats = sum([len(match) for match in matches])
+        # print(mystr,pattern,matches,sum_repeats)
+        if sum_repeats == mystrlen:
+            return num
+    return 0
+
+#  ins = [(x[0], int(x[1:])) for x in open('inputs/input_01_test.txt','r').read().splitlines()]
+# groups = open('inputs/input_02_test.txt','r').read().split(',')
+groups = open('inputs/input_02.txt','r').read().split(',')
+sum_invalids = 0
+for i,group in enumerate(groups):
+    AB = [int(x) for x in group.split('-')]
+    # print(AB)
+    IDs = list(range(AB[0],AB[1]+1))
+    # print(IDs)
+    for ID in IDs:
+        sum_invalids += does_it_repeat(ID)
+print(f'sum of invalid IDs = {sum_invalids}')
+
+# 50789015120 is too low
