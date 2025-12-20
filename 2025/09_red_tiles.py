5c2625385a71bce09f308c8ccfe564e77b581ab5
Dan Provenzano <danpro3@gmail.com>
2025-12-19 23:13:45 -0500

    2025 day 9 part 2 progress

--- 2025/09_red_tiles.py
+++ 2025/09_red_tiles.py
@@ -38,7 +38,6 @@ for i,line in enumerate(lines):
 print(f'Number of reds: {len(reds)}, reds: {reds[:20]}')
 
 
-
 # find areas, sort reverse
 areas = []
 for a in range(n-1):
@@ -47,7 +46,7 @@ for a in range(n-1):
             abs(reds[a][0] - reds[b][0]+1)*abs(reds[a][1] - reds[b][1]+1),\
             (reds[a][0],reds[a][1]),(reds[b][0],reds[b][1]) ))
 areas.sort(reverse=True)
-print(f'areas: {areas[:20]}')
+print(f'{len(areas)} areas: {areas[:20]}')
 
 # scale down the red coords
 x_values = [t[0] for t in reds]
@@ -56,12 +55,12 @@ y_values = [t[1] for t in reds]
 x_min = min(x_values)
 y_min = min(y_values)
 # reds = [(x-x_min,y-y_min) for x,y in reds]
-scale = 100 # 100
+scale = 50# 100
 reds = [(round((x-x_min)/scale)+1,round((y-y_min)/scale)+1) for x,y in reds]
 x_values = [t[0] for t in reds]
 y_values = [t[1] for t in reds]
 
-print(f'reds: {reds[:20]}')
+print(f'{len(reds)} reds: {reds[:20]}')
 print(f'x range: {min(x_values),max(x_values)}, y range: {min(y_values),max(y_values)}')
 # print(f'x values: {x_values[:20]}')
 # print(f'y values: {y_values[:20]}')
@@ -90,20 +89,9 @@ else:
     while x <= max(reds[0][0],reds[n-1][0]):
         greens.add((x,reds[n-1][1]))
         x += 1
-print(f'length of greens = {len(greens)}')
-print(f'greens: {greens}')
+print(f'{len(greens)} greens: {greens}')
 
 # better BFS get the coordinates of all tiles outside the loop
-
-# range: 
-# x_values = [t[0] for t in reds]
-# y_values = [t[1] for t in reds]
-# xrange = (min(x_values)-1,max(x_values)+1)
-# yrange = (min(y_values)-1,max(y_values)+1)
-# print(f'x range: {xrange}, y range: {yrange}')
-# print(f'x values: {x_values[:20]}')
-# print(f'y values: {y_values[:20]}')
-
 xrange = [0, max(x_values)+1]
 yrange = [0, max(y_values)+1]
 dirs = [(1,0),(0,1),(-1,0),(0,-1)]
@@ -128,6 +116,46 @@ while len(todo) > 0:
 print(f'length of visited = {len(visited)}')
 # print(visited)
 
+# ----  now loop thru rectangles
+def rectangle_boundary_points(A, B):
+    ABxmin, ABxmax = sorted([A[0], B[0]])
+    ABymin, ABymax = sorted([A[1], B[1]])
+    points = set()
+    for x in range(ABxmin, ABxmax + 1):
+        points.add((x, ABymin))
+        points.add((x, ABymax))
+    for y in range(ABymin, ABymax + 1):
+        points.add((ABxmin, y))
+        points.add((ABxmax, y))
+    return sorted(points)
+
+print(f'Looping thru {len(areas)} rectangles')
+found = False
+cnt = 0
+# for _ in range(90000):
+#     area, A, B = areas.pop(0)
+while not found:
+    area, A, B = areas.pop(90000)
+    cnt += 1
+    if cnt % 1000 == 0: 
+        print(f'cnt = {cnt}, checking area {area}')
+    A = (round((A[0]-x_min)/scale)+1,round((A[1]-y_min)/scale)+1)
+    B = (round((B[0]-x_min)/scale)+1,round((B[1]-y_min)/scale)+1)
+    points = rectangle_boundary_points(A, B)
+    if set(points).intersection(visited):
+        continue
+    else:
+        found = True
+        
+print(f'found area = {area}')
+# 1666288864 is too high (used scale 1000)
+
+# cnt = 90000, checking area 1716984150
+# found area = 1666288864 (scale 100)
+
+# cnt = 90000, checking area 1716984150
+# found area = 1573323843 (too low. scale = 50, also 10)
+
 # %%
 # make a grid
 grid = [['.']*(max(x_values)+2)]
@@ -140,6 +168,17 @@ for green in greens:
 print(f'Grid complete: {len(grid)} x {len(grid[0])}')
 _ = [print(''.join(x)) for x in grid]
 print()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -173,33 +212,7 @@ while found:
     y_min = min(A[1],B[1])
     y_max = max(A[1],B[1])
 
-
-
     this_rect = True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # %%
 # better BFS get the coordinates of all tiles outside the loop
