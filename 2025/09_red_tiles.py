d0c7d0fca977d7752055f818d2181732aa048434
Dan Provenzano <danpro3@gmail.com>
2025-12-26 02:06:24 -0500

    2025 day 09 part 2

--- 2025/09_red_tiles.py
+++ 2025/09_red_tiles.py
@@ -12,7 +12,7 @@ max_area = 0
 for a in range(n-1):
     for b in range(1,n):
         area = (abs(tiles[a][0] - tiles[b][0])+1)*\
-        abs((tiles[a][1] - tiles[b][1])+1)
+        (abs(tiles[a][1] - tiles[b][1])+1)
         if area > max_area:
             max_area = area
 
@@ -25,11 +25,8 @@ lines = open('inputs/input_09.txt','r').read().splitlines()
 '''
 make a list of red tile coordinates
 make a list of rectangles, sort by area
-make a list of green tile coordinates
-work thru the largest rectanges and test for all tiles
-inside being red or green
-    Do this for each row (column): check min (max) green is less(greater) than
-    the borders of the rectangle
+Add in some green tiles along the perimeter (only need the center line)
+work thru the largest rectanges and test any red/greens inside the rectangle
 '''
 n = len(lines)
 # list of reds
@@ -39,17 +36,15 @@ for i,line in enumerate(lines):
 print(f'Number of reds: {len(reds)}, reds: {reds[:20]}')
 print(f'{len(reds)} reds: {reds[:20]}')
 
-# %%
 # find areas, sort reverse
 areas = []
 for a in range(n-1):
     for b in range(1,n):
         areas.append((\
-            abs(reds[a][0] - reds[b][0]+1)*abs(reds[a][1] - reds[b][1]+1),\
+            (abs(reds[a][0] - reds[b][0])+1)*(abs(reds[a][1] - reds[b][1])+1),\
             (reds[a][0],reds[a][1]),(reds[b][0],reds[b][1]) ))
 areas.sort(reverse=True)
 print(f'{len(areas)} areas: {areas[:20]}')
-
 
 # add in some greens to the polygon
 # list of greens (greens + reds) make the border
@@ -59,24 +54,25 @@ for i in range(n-1):
         y = min(reds[i+1][1],reds[i][1])
         while y <= max(reds[i+1][1],reds[i][1]):
             greens.append((reds[i][0],y))
-            y += 500
+            y += 10000
     else:
         x = min(reds[i+1][0],reds[i][0])
         while x <= max(reds[i+1][0],reds[i][0]):
             greens.append((x,reds[i][1]))
-            x += 500
+            x += 10000
     greens.append(reds[i+1])
-    # wrap around
-# if reds[0][0] == reds[n-1][0]:
-#     y = min(reds[0][1],reds[n-1][1])
-#     while y <= max(reds[0][1],reds[n-1][1]):
-#         greens.append((reds[n-1][0],y))
-#         y += 100
-# else:
-#     x = min(reds[0][1],reds[n-1][1])
-#     while x <= max(reds[0][0],reds[n-1][0]):
-#         greens.append((x,reds[n-1][1]))
-#         x += 100
+
+# wrap around
+if reds[0][0] == reds[n-1][0]:
+    y = min(reds[0][1],reds[n-1][1])
+    while y <= max(reds[0][1],reds[n-1][1]):
+        greens.append((reds[n-1][0],y))
+        y += 100
+else:
+    x = min(reds[0][0],reds[n-1][0])
+    while x <= max(reds[0][0],reds[n-1][0]):
+        greens.append((x,reds[n-1][1]))
+        x += 100
 print(f'{len(greens)} greens: {greens[:20]}')
 
 # Build the list of all points
@@ -86,12 +82,6 @@ Xs = [t[0] for t in greens]
 Ys = [t[1] for t in greens]
 print(f'x range: {min(Xs),max(Xs)}, y range: {min(Ys),max(Ys)}')
 
-# plot
-# import matplotlib.pyplot as plt
-# plt.plot(Xs,Ys,'x')
-# plt.plot(Xs,Ys)
-
-
 # loop thru rectangles, check for any greens inside
 print(f'Looping thru {len(areas)} rectangles')
 good_rect = False
@@ -99,7 +89,7 @@ cnt = 0
 while not good_rect:
     good_rect = True
     # area, A, B = areas.pop(0)
-    area, A, B = areas.pop(90000+7000)
+    area, A, B = areas.pop(91000)
     cnt += 1
     if cnt % 1000 == 0: # % 1000
         print(f'cnt = {cnt}, checking area {area, A, B} ')
@@ -107,25 +97,22 @@ while not good_rect:
     y_rect = (min(A[1],B[1]), max(A[1],B[1]))
     # find all relevent red points
     indx = [index for index, value in enumerate(Xs) if x_rect[0] < value < x_rect[1]]
-    indy = [index for index, value in enumerate(Ys) if y_rect[0] < value < y_rect[1]]
+    # indy = [index for index, value in enumerate(Ys) if y_rect[0] < value < y_rect[1]]
 
     # print(f'                         x_rect = {x_rect}, y_rect = {y_rect}')
     # check that the reds are at least at the boundary of the rectangle
-    # for indx1 in indx:
-    #     if y_rect[0] < Ys[indx1] < y_rect[1]:
-    #         good_rect = False
-    # if good_rect == True:
-    #     for indy1 in indy:
-    #         if x_rect[0] < Xs[indy1] < x_rect[1]:
-    #             good_rect = False
-    # if cnt == 20000:
-    #     good_rect = True
-
+    for indx1 in indx:
+        if y_rect[0] < Ys[indx1] < y_rect[1]:
+            good_rect = False
+    # good_rect = True
 
 print(f'found {good_rect} cnt = {cnt}, area = {area}, A = {A}, B = {B}')
+import matplotlib.pylab as plt
 # plt.plot(Xs,Ys,'x',A[0],A[1],'ro',B[0],B[1],'r*')
-plt.plot(Xs,Ys,A[0],A[1],'ro',B[0],B[1],'r*')
-
+plt.plot(Xs,Ys,'.',A[0],A[1],'ro',B[0],B[1],'r*')
+plt.plot(A[0],B[1],'yo',B[0],A[1],'y*')
+plt.axis('equal')
+plt.show()
 # 1666288864 is too high (used scale 1000)
 
 # cnt = 90000, checking area 1716984150
@@ -133,10 +120,76 @@ plt.plot(Xs,Ys,A[0],A[1],'ro',B[0],B[1],'r*')
 
 # cnt = 90000, checking area 1716984150
 # found area = 1573323843 (too low. scale = 50, also 10)
-
 # cnt = 90000+2499, found area = 1661338600 (too high
 #  scale=50)
 
+# cnt = 1860, area = 1652426442 (not right), A = (6216, 30095), B = (94876, 48734)
+# cnt = 2304, area = 1641001348 (not right), A = (6829, 30095), B = (94876, 48734)
+# cnt = 3412, area = 1614351200 (not right), A = (4939, 32107), B = (94876, 50058)
+# cnt = 5370, area = 1574305000 (not right), A = (5375, 31143), B = (94876, 48734)
 
 
-# %%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %% UNUSED:
+
+# make a grid
+grid = [['.']*(max(x_values)+2)]
+for _ in range(max(y_values)+1):
+    grid.append(['.']*(max(x_values)+2))
+for green in greens:
+    grid[green[1]][green[0]] = '#'
+# for vis in visited:
+#     grid[vis[1]][vis[0]] = 'x'
+print(f'Grid complete: {len(grid)} x {len(grid[0])}')
+_ = [print(''.join(x)) for x in grid]
+print()
+
+
+
+# better BFS get the coordinates of all tiles outside the loop
+
+# range: 
+x_values = [t[0] for t in reds]
+y_values = [t[1] for t in reds]
+xrange = (min(x_values)-1,max(x_values)+1)
+yrange = (min(y_values)-1,max(y_values)+1)
+print(f'x range: {xrange}, y range: {yrange}')
+print(f'x values: {x_values[:20]}')
+print(f'y values: {y_values[:20]}')
+
+dirs = [(1,0),(0,1),(-1,0),(0,-1)]
+def move(todo):
+    x,y = todo.pop(0)
+    if (x,y) in visited:
+        return
+    else:
+        visited.add((x,y))
+        for dir in dirs:
+            if x+dir[0] >= xrange[0] and x+dir[0] <= xrange[1]\
+            and y+dir[1] >= yrange[0] and y+dir[1] <= yrange[1]\
+            and (x+dir[0],y+dir[1]) not in greens:
+                todo.append((x+dir[0],y+dir[1]))
+            # and (x+dir[0] in x_values or y+dir[1] in y_values):
+
+visited = set()
+todo = [(xrange[0],yrange[0])]
+while len(todo) > 0:
+    # print(todo)
+    move(todo)
+print(f'length of visited = {len(visited)}')
+# print(visited)
