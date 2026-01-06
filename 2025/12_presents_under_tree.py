e3630ff0c0f3f5b4bcfbc03e1c2595c3d6f39b3d
Dan Provenzano <danpro3@gmail.com>
2026-01-05 23:20:40 -0500

    2025 day 11 part 2

--- 2025/12_presents_under_tree.py
+++ 2025/12_presents_under_tree.py
@@ -51,9 +51,11 @@ def rot(piece:set[tuple[int, int]],dir) -> set[tuple[int, int]]: # dir = +1 for 
     return {CCW[xy] for xy in piece}
 
 # %% 
-def mini_BFS(extras: set) -> int:
+def mini_BFS(layout: set) -> int:
+    extras = all_spots - layout
     dirs = [(1,0),(0,1),(-1,0),(0,-1)]
     unusables: int = 0
+    # print(f'start: layout: {len(layout)}, extras: {len(extras)}, unusables: {unusables}')
     while extras:
         XX,YY = extras.pop()
         quickvisited: set[tuple[int, int]] = set()
@@ -64,62 +66,85 @@ def mini_BFS(extras: set) -> int:
                 continue
             quickvisited.add((x,y))
             for dir in dirs:
-                if x+dir[0] >= 0 and x+dir[0] <= Xlim\
-                and y+dir[1] >= 0 and y+dir[1] <= Ylim\
+                if x+dir[0] >= 0 and x+dir[0] < Xlim\
+                and y+dir[1] >= 0 and y+dir[1] < Ylim\
                 and (x+dir[0],y+dir[1]) not in layout:
                     todo.append((x+dir[0],y+dir[1]))
+        if len(quickvisited) < 7:
+            unusables += len(quickvisited)
+            # print(f'unusables: {unusables}')
         extras = extras - quickvisited
-        unusables += len(quickvisited)
-    print(f'unusables: {unusables}')
+    # print(f'unusables: {unusables}')
+    # print(f'end: layout: {len(layout)}, extras: {len(extras)}, unusables: {unusables}')
     return unusables
 
 
 def BFS(piece_pile: list):
-    visited: set[tuple[tuple[tuple[int, int], ...], int, tuple[tuple[int, int], ...]]] = set()
-    todo: list[tuple[set, int, set, set]] = []
+    # visited: set[tuple[tuple[tuple[int, int], ...], int, tuple[tuple[int, int], ...]]] = set()
+    visited: set[tuple[tuple[int, int],...]] = set()
+    todo: list[tuple[set, int, set]] = []
     layout: set[tuple[int, int]] = set()
     cnt = 0
     this_piece = P[piece_pile[cnt]]
     # add the first piece, all orientations with an empty layout
     for dx in range(Xlim-2):
         for dy in range(Ylim-2):
-            # every flip
-            for f in range(2):
-                # every rotaion
-                for r in range(4):
+            # every rotaion
+            for r in range(4):
+                # every flip
+                for f in range(2):
                     new_piece = flip(this_piece,f)
                     new_piece = rot(new_piece,r)
                     new_piece = move(new_piece,(dx,dy))
-                    todo.append((layout, cnt, new_piece, all_spots))
-
+                    todo.append((layout, cnt, new_piece))
+    
+    print(f'len layout: {len(layout)}, len todo: {len(todo)}')
+    counter = 0
+    unusables = 0
     while todo:
+        if counter % 10000 == 0:
+            print(f'counter: {counter}, cnt: {cnt}, len layout: {len(layout)}, len todo: {len(todo)}')
+        counter += 1
         # _=[print(x) for x in todo]
-        layout, cnt, this_piece, extras = todo.pop(-1)
+        layout, cnt, this_piece = todo.pop(-1)
         # print(f'    pop. len layout: {len(layout)}')
+        # if counter == 239:
+        #     print(f'counter: {counter}, cnt: {cnt}, len layout: {len(layout)}, len todo: {len(todo)}')
 
-        # check the cache
-        if (tuple(layout), cnt, tuple(this_piece)) in visited:
-            continue
-        else:
-            visited.add((tuple(sorted(layout)), cnt, tuple(sorted(this_piece))))           
+        # if cnt == 2:
+        #     print(f'counter: {counter}, cnt: {cnt}, len layout: {len(layout)}, len todo: {len(todo)}')
+        #     break
 
-        # check the unusables
-        print(f'len layout: {len(layout)}')
-        unusables = mini_BFS(extras)
-        # if unusables > needed - len(layout):
-        print(f'unusables: {unusables}')
-        #     continue
-        
         # check for fit
         if any(coord in layout for coord in this_piece):
+            continue
+
+        # check the cache
+        # if (tuple(sorted(layout)), cnt, tuple(sorted(this_piece))) in visited:
+        #     continue
+        # else:
+        #     visited.add((tuple(sorted(layout)), cnt, tuple(sorted(this_piece))))           
+        
+        new_layout = layout.union(this_piece)
+        if tuple(sorted(new_layout)) in visited:
+            continue
+        else:
+            visited.add(tuple(sorted(new_layout)))
+
+        # check the unusables
+        # print(f'len layout: {len(layout)}')
+        unusables = mini_BFS(layout)
+        # print(f'unusables: {unusables}')
+        if unusables > needed - len(layout):
             continue
         
         # check for done
         if cnt == len(piece_pile)-1: # and not any(coord in layout for coord in this_piece):
-            return layout.union(this_piece), cnt+1, True
+            return layout.union(this_piece), cnt+1, True, unusables
 
         # print(f'len todo: {len(todo)}, current cnt: {cnt}')
         # place it and add the new piece
+    
         for dx in range(Xlim-2):
             for dy in range(Ylim-2):
                 # every rotaion
@@ -129,15 +154,17 @@ def BFS(piece_pile: list):
                         new_piece = flip(P[piece_pile[cnt+1]],f)
                         new_piece = rot(new_piece,r)
                         new_piece = move(new_piece,(dx,dy))
-                        todo.append((layout.union(this_piece), cnt+1, new_piece, all_spots.intersection(layout.union(this_piece))))
-    return layout, cnt, False
+                        todo.append((new_layout, cnt+1, new_piece))
+                        # print(f'len layout: {len(layout)}, len todo: {len(todo)}')
 
-for tree in [trees[1]]:
+    return layout, cnt, False, unusables
+
+for tree in [trees[2]]:
     print(tree)
     Xlim = tree['XY'][0]
     Ylim = tree['XY'][1]
+    # Xlim = 5; Ylim = 4
     all_spots: set[tuple[int, int]] = set([(x,y) for x in range(Xlim) for y in range(Ylim)])
-    # Xlim = 6; Ylim = 6
     piece_pile = []
     for i,n_pieces in enumerate(tree['piece_list']):
         for cnt in range(n_pieces):
@@ -149,9 +176,10 @@ for tree in [trees[1]]:
     # piece_pile = piece_pile[::-1]
     # piece_pile = [4,4,1,4,3]
     print(f'piece list: {tree["piece_list"]}, piece_pile: {piece_pile}')
-    layout, cnt, can_fit = BFS(piece_pile)
-    print(f'len layout: {len(layout)}, cnt: {cnt}, can fit: {can_fit}')
-
+    layout, cnt, can_fit, unusables = BFS(piece_pile)
+    print(f'len layout: {len(layout)}, cnt: {cnt}, can fit: {can_fit}, unusables: {unusables}')
+    unusables = mini_BFS(layout)
+    print(f'unusables: {unusables}')
  
     ax = plt.figure().gca()
     print_grid(layout)
@@ -161,4 +189,3 @@ for tree in [trees[1]]:
     plt.show()
 
     
-# %%
